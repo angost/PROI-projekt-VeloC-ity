307d5a357bf7eb79d27472b47c206d657bff99e1
aostrows <01178572@pw.edu.pl>
2023-05-11 18:26:32 +0200

    Use example

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
 
+#include "Vehicle.h"
+#include "Bike.h"
+#include "Scooter.h"
+#include "ElectricScooter.h"
+#include <iostream>
+
+int main() {
+
+	cout << "Bike" << endl;
+	Bike bike0(0);
+	cout << bike0.checkRentedStatus() << endl;
+	bike0.setRentedStatus(true);
+	cout << bike0.checkRentedStatus() << endl;
+	bike0.takeOff();
+
+
+	cout << endl << "Scooter" << endl;
+	Scooter scooter1(1);
+	cout << scooter1.checkRentedStatus() << endl;
+	scooter1.setRentedStatus(true);
+	cout << scooter1.checkRentedStatus() << endl;
+	scooter1.takeOff();
+
+	return 0;
+}
