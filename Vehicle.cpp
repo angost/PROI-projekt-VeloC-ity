831ac27cdba38cbd609b3f23626b1c8ddfabf6b4
Angelika Ostrowska <01178572@pw.edu.pl>
2023-05-11 14:41:10 +0200

    Changed Vehicl's responsibilities

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-Vehicle::Vehicle(int id, Station currentStation, Location location, int numberOfRentals, int technicalCondition)
+Vehicle::Vehicle(int id, int numberOfRentals, int technicalCondition)
 {
 	if (id < 0)
 		throw invalid_argument("Id cannot be negative");
@@ -11,18 +11,6 @@ Vehicle::Vehicle(int id, Station currentStation, Location location, int numberOf
 
 	rentalPrice = 5;
 	maxSpeed = 20;
-
-	// na poczatku nie powinno byc usera -> zrobic User None? -> empty constructor usera tworzy noUsera?
-	User noUser();
-	currentUser = noUser;
-	//
-
-	if (currentStation.countAvailableSpots() == 0)
-		throw invalid_argument("No available spots on chosen station");
-	this->currentStation = currentStation;
-
-	this->location = location;
-
 	rented = false;
 	reserved = false;
 
@@ -30,7 +18,7 @@ Vehicle::Vehicle(int id, Station currentStation, Location location, int numberOf
 		throw invalid_argument("Number of rentals cannot be negative");
 	this->numberOfRentals = numberOfRentals;
 
-	// technicalCondition od 0 do 5, gdy spadnie do 0 jest unable to use ?
+	// technicalCondition od 0 do 5, gdy spadnie do 0 jest unable to use
 	if (technicalCondition < 0 || technicalCondition > 5)
 		throw invalid_argument("Technical condition has to be between 0 and 5");
 	this->technicalCondition = technicalCondition;
@@ -51,35 +39,6 @@ void Vehicle::setReservedStatus(bool newStatus)
 	this->reservedStatus = newStatus;
 }
 
-void Vehicle::setUser(User user)
-{
-	this->currentUser = user;
-}
-
-void Vehicle::removeUser()
-{
-	User noUser();
-	this->currentUser = noUser;
-
-}
-
-void Vehicle::setStation(Station station)
-{
-	this->currentStation = station;
-}
-
-void Vehicle::removeStation()
-{
-	// znowu, co jak stacja pusta?
-	Station noStation();
-	this->currentStation = noStation;
-}
-
-void Vehicle::setLocation(Location location)
-{
-	this->location = location;
-}
-
 void Vehicle::increaseNumberOfRentals()
 {
 	this->numberOfRentals++;
@@ -96,47 +55,22 @@ void Vehicle::setTechnicalCondition(int technicalCondition)
 		ableToUse = true;
 }
 
-bool Vehicle::reserveVehicle(User user)
+bool Vehicle::checkRentedStatus()
 {
-	// w Userze - sprawdzenie czy ma odpowiedni balance, czy nie ma pelnego limitu wypozyczonych pojazdow
-	// w Userze/Vehicle - czy ten vehicle jest juz zarezerwowany/pozyczony
-	this->setReservedStatus(true);
-	this->setUser(user);
-	//dodaj do zarezerwowanych pojazdow usera?
-	return true;
+	return this->rentedStatus;
 }
 
-bool Vehicle::cancelReserveVehicle(User user)
+bool Vehicle::checkReservedStatus()
 {
-	// w Userze/Vehicle - czy ten vehicle jest zarezerwowany przez tego usera
-	this->setReservedStatus(false);
-	this->removeUser();
-	//usun z zarezerwowanych pojazdow usera?
-	return true;
+	return this->reservedStatus;
 }
 
-bool Vehicle::rentVehicle(User user)
+int Vehicle::checkTechnicalCondition()
 {
-	// w Userze - sprawdzenie czy ma odpowiedni balance, czy jest na stacji takiej jak ten vehicle, czy vehicle nie jest zarezerwowany/jest przez tego usera
-	// w Vehicle - zmiana statusu, zwiekszenie liczby wypozyczen, ustawienie uzytkownika, usuniecie ze stacji
-	this->setRentedStatus(true);
-	this->setReservedStatus(false);
-	this->increaseNumberOfRentals();
-	this->setUser(user);
-	user.addVehicle(this);
-	this->currentStation.deleteVehicle(this);
-	return true;
+	return this->technicalCondition;
 }
 
-
-bool Vehicle::returnVehicle(Station station)
+bool Vehicle::checkAbleToUse()
 {
-	// w Userze - czy jest na stacji, czy na stacji sa miejsca
-	// w Vehicle - zmien status, usun usera, dodaj do stacji
-	this->setRentedStatus(false);
-	this->removeUser();
-	user.removeVehicle(this);
-	this->currentStation = station;
-	station.addToStation(this);
-	return true;
+	return this->ableToUse;
 }
--- Vehicle.h
+++ Vehicle.h
@@ -13,31 +13,19 @@ private:
     int maxSpeed;
     bool rentedStatus;
     bool reservedStatus;
-    User currentUser;
-    Station currentStation;
-    Location currentLocation;
     int numberOfRentals;
     int technicalCondition;
     bool ableToUse;
 
 public:
-    Vehicle(int id, Station currentStation, Location currentLocation, int numberOfRentals = 0, int technicalCondition = 5);
-    // co jak aktualnie nie ma usera? obiekt User reprezentuj¹cy no user?
+    Vehicle(int id, int numberOfRentals = 0, int technicalCondition = 5);
     void setRentedStatus(bool newStatus);
     void setReservedStatus(bool newStatus);
-    void setUser(User user);
-    void removeUser();
-    void setStation(Station station);
-    void removeStation();
-    void setLocation(Location location);
-    void increaseNumberOfRentals();
     void setTechnicalCondition(int technicalCondition);
-    bool reserveVehicle(User user);
-    bool cancelReserveVehicle(User user);
-    bool rentVehicle(User user);
-    bool returnVehicle(Station station);
-    // robic gettery czy nie?
-    // ktore void ktore bool, gdzie potrzebujemy info zwrotne czy sie udalo?
+    bool checkRentedStatus();
+    bool checkReservedStatus();
+    int checkTechnicalCondition();
+    bool checkAbleToUse();
 };
 
 
