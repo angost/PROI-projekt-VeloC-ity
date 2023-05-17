8d30cbfeca73f59239a84f4e65a3ecb18c885da9
Adrian Murawski <01178568@pw.edu.pl>
2023-05-17 22:03:27 +0200

    usage for class Station

--- main.cpp
+++ main.cpp
@@ -3,18 +3,19 @@
 #include "vehicle/Scooter.h"
 #include "vehicle/ElectricScooter.h"
 
-#include "station/station/Station.h"
 #include "station/station/MainStation.h"
+#include "station/station/SubStation.h"
+#include "station/station/LocalStation.h"
 
 #include "Location.h"
 #include <iostream>
 #include <vector>
 using namespace std;
 
-
-void testInhStation(){
+void testStation(){
+    // MainStation
     vector < Vehicle* > vec;
-    MainStation myMainStation("testname", "A01", Location("Warsaw", "Mokotow", "Ogrodowa", "1B", 0, 10), vec);
+    MainStation myMainStation("testname", "A01", Location("Warsaw", "Mokotow", "Ogrodowa", "1A", 0, 10), vec);
     myMainStation.getDesc();
     Bike bike(1);
     Scooter scooter(2);
@@ -23,8 +24,37 @@ void testInhStation(){
     for (auto i : myMainStation){
         cout << i->id << endl;
     }
-}
+    cout << "Repair station: " << myMainStation.repairStation << endl;
+    cout << "Wheel pump: " << myMainStation.wheelPump << endl;
 
+    cout << endl;
+    // SubStation
+    vector < Vehicle* > vecSub;
+    SubStation mySubStation("testnameSub", "B01", Location("Warsaw", "Mokotow", "Ogrodowa", "1B", 0, 20), vecSub);
+    mySubStation.getDesc();
+    Bike bikeSub(3);
+    Scooter scooterSub(4);
+    mySubStation.addToStation(&bikeSub);
+    mySubStation.addToStation(&scooterSub);
+    for (auto i : mySubStation){
+        cout << i->id << endl;
+    }
+    cout << "Vending machine: " << mySubStation.vendingMachine << endl;
+
+    cout << endl;
+    // LocalStation
+    vector < Vehicle* > vecLocal;
+    LocalStation myLocalStation("testnameLoc", "C01", Location("Warsaw", "Mokotow", "Ogrodowa", "1C", 0, 30), vecLocal);
+    myLocalStation.getDesc();
+    Bike bikeLocal(5);
+    Scooter scooterLocal(6);
+    myLocalStation.addToStation(&bikeLocal);
+    myLocalStation.addToStation(&scooterLocal);
+    for (auto i : myLocalStation){
+        cout << i->id << endl;
+    }
+    cout << "Emergency phone: " << myLocalStation.emergencyPhone << endl;
+}
 
 void print(Vehicle* vehicle)
 {
@@ -33,7 +63,7 @@ void print(Vehicle* vehicle)
 
 int main() {
 
-    testInhStation();
+    testStation();
 
 //	Bike bike0(0);
 //	print(&bike0);
