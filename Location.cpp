2835dd61ac28c04b827ba6972cb0f6941fe6c421
Adrian Murawski <01178568@pw.edu.pl>
2023-05-30 14:45:34 +0200

    calculating distance in Location class

--- Location.cpp
+++ Location.cpp
@@ -1,4 +1,5 @@
 #include "Location.h"
+#include <algorithm>
 
 Location::Location(string city, string district, string street_name, string street_number, int x_coord, int y_coord) {
 	this->city = city;
@@ -7,4 +8,13 @@ Location::Location(string city, string district, string street_name, string stre
 	this->street_number = street_number;
 	this->x_coord = x_coord;
 	this->y_coord = y_coord;
+}
+
+int Location::getDistanceBetweenLocations(Location other) {
+    int distance = 0;
+    int x_distance = other.x_coord - this->x_coord;
+    distance += max(x_distance, -x_distance);
+    int y_distance = other.y_coord - this->y_coord;
+    distance += max(y_distance, -y_distance);
+    return distance;
 }
