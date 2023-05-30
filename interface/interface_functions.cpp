322aa51490838b39e5de0c090b3d7b12147a200d
Adrian Murawski <01178568@pw.edu.pl>
2023-05-30 15:59:40 +0200

    created interface functions

---
+++ interface/interface_functions.cpp
@@ -0,0 +1,51 @@
+//
+// Created by Adrian Murawski on 30/05/2023.
+//
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <vector>
+
+
+using namespace std;
+
+string CREDENTIAL_FILE_NAME = "data/credentials.txt";
+
+void loginInterface(string &username, string &password){
+    cout << "Enter username >> ";
+    cin >> username;
+    cout << "Enter password >> ";
+    cin >> password;
+}
+
+bool checkCredentials(const map <string, string >& credentials, const string& username, const string& password){
+    for (const auto& pair : credentials){
+        if (username == pair.first){
+            if (password == pair.second) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+    return false;
+}
+
+
+map < string, string > getAllCredentials(){
+    map < string, string > credentials;
+    ifstream file(CREDENTIAL_FILE_NAME);
+    string line;
+
+    while (getline(file, line)) { // Odczytanie kolejnych linii z pliku
+        istringstream iss(line);
+        string username, password;
+        if (iss >> username >> password) {
+            credentials[username] = password;
+        }
+    }
+    return credentials;
+}
+
+void mainScreenInterface();
