4e236e5f108e5b912e1153c69c627cafe297bdee
Adrian Murawski <01178568@pw.edu.pl>
2023-06-05 23:40:49 +0200

    changed DataParser to use new operators<< and >> in Station

--- data/DataParser.cpp
+++ data/DataParser.cpp
@@ -26,43 +26,33 @@ vector<Station *> DataParser::getAllStations() {
 Station* DataParser::getStation(const string& filename) {
     std::ifstream file(filename);
     std::string line;
-    std::string stationType, name, code, city, district, streetName, streetNumber;
-    int x, y;
     string vehicleType;
-    int id;
 
     if (file.is_open()) {
+        string type;
         getline(file, line);
-        std::istringstream stationData(line);
-        stationData >> stationType >> name >> code >> city >> district >> streetName >> streetNumber >> x >> y;
-        Location stationLocation(city, district, streetName, streetNumber, x, y);
-        vector<Vehicle*> stationVehicles;
-        while (std::getline(file, line)) {
-            std::istringstream iss(line);
-            iss >> vehicleType >> id;
-            if (vehicleType == "Bike") {
-                auto* vehicle = new Bike(id);
-                stationVehicles.push_back(vehicle);
-            } else if (vehicleType == "Scooter") {
-                auto* vehicle = new Scooter(id);
-                stationVehicles.push_back(vehicle);
-            } else if (vehicleType == "ElectricScooter") {
-                auto* vehicle = new ElectricScooter(id);
-                stationVehicles.push_back(vehicle);
-            } else {
-                throw std::invalid_argument("Wrong vehicle type");
-            }
-        }
+        std::istringstream iss(line);
+        iss >> type;
         file.close();
-        if (stationType == "MainStation") {
-            auto* station = new MainStation(name, code, stationLocation, stationVehicles);
-            return station;
-        } else if (stationType == "SubStation") {
-            auto* station = new SubStation(name, code, stationLocation, stationVehicles);
-            return station;
-        } else if (stationType == "LocalStation") {
-            auto* station = new LocalStation(name, code, stationLocation, stationVehicles);
-            return station;
+        std::ifstream sameFile(filename);
+        if (type == "MainStation") {
+            auto* mainStation = new MainStation;
+            sameFile >> *mainStation;
+            file.close();
+            return mainStation;
+        } else if (type == "SubStation") {
+            auto* subStation = new SubStation;
+            sameFile >> *subStation;
+            file.close();
+            return subStation;
+        } else if (type == "LocalStation") {
+            auto* localStation = new LocalStation;
+            sameFile >> *localStation;
+            file.close();
+            return localStation;
+        } else {
+            file.close();
+            throw invalid_argument("Wrong station type");
         }
     }
     return nullptr;
