90f24175e23e3f20de4d59b9e8a1cb3eaa875c7c
Adrian Murawski <01178568@pw.edu.pl>
2023-06-05 16:52:18 +0200

    changed main - stations read from files

--- main.cpp
+++ main.cpp
@@ -23,7 +23,10 @@
 #include "interface/interface_functions.h"
 #include "interface/UserInterface.h"
 #include "interface/ServiceInterface.h"
+
+
 #include "src/InputParser.h"
+#include "data/DataParser.h"
 
 #include <iostream>
 #include <vector>
@@ -33,69 +36,23 @@
 
 using namespace std;
 
+string STATIONS_DATA_PATH = "../data/stationsData";
 
 int main(int argc, char **argv) {
     // DATA
-    vector < Vehicle* > mainVeh;
-    vector < Vehicle* > subVeh;
-    vector < Vehicle* > localVeh;
-    MainStation myMainStation("MainStation", "A01", Location("Warsaw", "Mokotow", "Ogrodowa", "1A", 0, 10), mainVeh);
-    SubStation mySubStation("SubStation", "B01", Location("Warsaw", "Mokotow", "Ogrodowa", "1B", 0, 20), subVeh);
-    LocalStation myLocalStation("LocalStation", "C01", Location("Warsaw", "Mokotow", "Ogrodowa", "1C", 0, 30), localVeh);
-    Bike bike1(1);
-    Bike bike2(2);
-    Bike bike3(3);
-    Bike bike4(4);
-    Bike bike5(5);
-    Bike bike6(6);
-    Bike bike7(7);
-    Bike bike8(8);
-    myLocalStation.addToStation(&bike1);
-    myLocalStation.addToStation(&bike2);
-    myLocalStation.addToStation(&bike3);
-    myLocalStation.addToStation(&bike4);
-    myLocalStation.addToStation(&bike5);
-    myLocalStation.addToStation(&bike6);
-    myLocalStation.addToStation(&bike7);
-    myLocalStation.addToStation(&bike8);
-    Scooter scooter9(9);
-    Scooter scooter10(10);
-    Scooter scooter11(11);
-    Scooter scooter12(12);
-    Scooter scooter13(13);
-    Scooter scooter14(14);
-    Scooter scooter15(15);
-    Scooter scooter16(16);
-    mySubStation.addToStation(&scooter9);
-    mySubStation.addToStation(&scooter10);
-    mySubStation.addToStation(&scooter11);
-    mySubStation.addToStation(&scooter12);
-    mySubStation.addToStation(&scooter13);
-    mySubStation.addToStation(&scooter14);
-    mySubStation.addToStation(&scooter15);
-    mySubStation.addToStation(&scooter16);
-    ElectricScooter elScooter17(17);
-    ElectricScooter elScooter18(18);
-    ElectricScooter elScooter19(19);
-    ElectricScooter elScooter20(20);
-    ElectricScooter elScooter21(21);
-    ElectricScooter elScooter22(22);
-    ElectricScooter elScooter23(23);
-    ElectricScooter elScooter24(24);
-    myMainStation.addToStation(&elScooter17);
-    myMainStation.addToStation(&elScooter18);
-    myMainStation.addToStation(&elScooter19);
-    myMainStation.addToStation(&elScooter20);
-    myMainStation.addToStation(&elScooter21);
-    myMainStation.addToStation(&elScooter22);
-    myMainStation.addToStation(&elScooter23);
-    myMainStation.addToStation(&elScooter24);
-
-    vector < Station* > stations = {&myMainStation, &mySubStation};
-    vector < Station* > smallerStations = {&myLocalStation};
+    vector < string > filenames = {STATIONS_DATA_PATH + "/station1.txt",
+                                   STATIONS_DATA_PATH + "/station2.txt",
+                                   STATIONS_DATA_PATH + "/station3.txt",
+                                   STATIONS_DATA_PATH + "/station4.txt",
+                                   STATIONS_DATA_PATH + "/station5.txt"};
+    DataParser data(filenames);
+    vector < Station* > stations = data.getAllStations();
+    vector < Station* > bigCrew = {stations[0], stations[1], stations[2], stations[3], stations[4]};
+    vector < Station* > smallCrew = {stations[2]};
     vector < Station* > adminStations;
-    Service firstServiceCrew("S01", stations);
-    Service secondServiceCrew("S91", smallerStations);
+
+    Service firstServiceCrew("S01", bigCrew);
+    Service secondServiceCrew("S91", smallCrew);
     vector < Service > serviceCrews = {firstServiceCrew, secondServiceCrew};
     AdminService admin("X01", adminStations, serviceCrews);
 
