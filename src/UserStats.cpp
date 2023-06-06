2c8277c46bf71b417963f65fcfe41fbcb10bcf82
Kacper Straszak <01178602@pw.edu.pl>
2023-06-06 15:52:25 +0200

    created userStats class

---
+++ src/UserStats.cpp
@@ -0,0 +1,8 @@
+//
+// Created by k3acp on 06.06.2023.
+//
+
+#include "UserStats.h"
+
+UserStats::UserStats(string username, string userClass, int resVehicles, int rentVehicles, float balance, string drivingLicense) :
+    username(username), userClass(userClass), resVehicles(resVehicles), rentVehicles(rentVehicles), balance(balance), drivingLicense(drivingLicense){}
